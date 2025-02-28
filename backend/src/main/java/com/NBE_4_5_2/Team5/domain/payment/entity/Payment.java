6cb242ae347ad0dd92c460c29b944079aaa0f48a
omegafrog <jiwoo22110103@gmail.com>
2025-02-28 17:42:04 +0900

    feat: date auditing을 위해 @EntityListeners 추가

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/payment/entity/Payment.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/payment/entity/Payment.java
@@ -2,21 +2,21 @@ package com.NBE_4_5_2.Team5.domain.payment.entity;
 
 import com.NBE_4_5_2.Team5.domain.member.entity.Member;
 import com.NBE_4_5_2.Team5.domain.payment.enums.PaymentStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class Payment {
 
     @Id
@@ -34,6 +34,7 @@ public class Payment {
     @LastModifiedDate
     private LocalDateTime modifiedAt;
 
+    @Enumerated(EnumType.STRING)
     private PaymentStatus status;
 
     @Builder
@@ -53,7 +54,7 @@ public class Payment {
     };
 
     /**
-     * 결제 요청 전에 저장한 payment메타데이터의 {@code totalAmount}와 인자로 들어오는 {@code amount}가 같은지 비교합니다.
+     * 결제 요청 전에 저장한 payment 메타데이터의 {@code totalAmount}와 인자로 들어오는 {@code amount}가 같은지 비교합니다.
      * @param amount 결제 요청시 클라이언트가 전달한 총 지불 금액
      * @return 요청 전에 저장한 데이터와 인자로 받은 총 가격이 동일하다면 {@code true}, 그렇지 않다면 {@code false}를 반환합니다.
      */
