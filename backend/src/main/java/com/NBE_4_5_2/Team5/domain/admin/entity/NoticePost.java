586416bd27c0f5e18f3c19ba2fe7d870667cfd5d
omegafrog <jiwoo22110103@gmail.com>
2025-03-02 15:54:39 +0900

    feat: jpaAuditing 추가

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/admin/entity/NoticePost.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/admin/entity/NoticePost.java
@@ -5,10 +5,12 @@ import java.util.UUID;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.NBE_4_5_2.Team5.domain.user.entity.User;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -21,6 +23,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "notice_post")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class NoticePost {
 	@Id
 	private final String id = "npost-" + UUID.randomUUID();
