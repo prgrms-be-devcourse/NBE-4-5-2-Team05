8a0b2a9ec6afce7beb307c4e3851fa965858fc58
omegafrog <jiwoo22110103@gmail.com>
2025-03-04 10:51:52 +0900

    feat: banList repository 작성

---
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/admin/repository/BanListRepository.java
@@ -0,0 +1,8 @@
+package com.NBE_4_5_2.Team5.domain.admin.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.NBE_4_5_2.Team5.domain.admin.entity.BanList;
+
+public interface BanListRepository extends JpaRepository<BanList, String> {
+}
