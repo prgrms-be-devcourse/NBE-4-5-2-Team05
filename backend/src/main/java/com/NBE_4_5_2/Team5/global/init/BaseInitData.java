2f5dce707feef8357d06c2fc2a729b60a309b74e
omegafrog <jiwoo22110103@gmail.com>
2025-03-05 11:00:56 +0900

    feat: 관리자 샘플 데이터 추가

--- backend/src/main/java/com/NBE_4_5_2/Team5/global/init/BaseInitData.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/global/init/BaseInitData.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.NBE_4_5_2.Team5.domain.user.repository.UserRepository;
+import com.NBE_4_5_2.Team5.domain.admin.service.AdminService;
 import com.NBE_4_5_2.Team5.domain.user.service.UserService;
 
 import jakarta.transaction.Transactional;
@@ -15,7 +15,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Profile("!test")
 public class BaseInitData {
-    private final UserService userService;
+	private final UserService userService;
+	private final AdminService adminService;
 
 	@Bean
 	public ApplicationRunner applicationRunner() {
@@ -25,15 +26,18 @@ public class BaseInitData {
 	@Transactional
 	public void userInit() {
 
-        if (userService.count() > 0) {
-            return;
-        }
+		if (userService.count() > 0) {
+			return;
+		}
 
+		userService.signup("user1", "user11234@", "user1@gmail.com", "user1", "서울시 강남구",
+			"https://example.com/default_profile.png");
+		userService.signup("user2", "user21234@", "user2@gmail.com", "user2", "서울시 강서구",
+			"https://example.com/default_profile.png");
+		userService.signup("user3", "user31234@", "user3@gmail.com", "user3", "서울시 광진구",
+			"https://example.com/default_profile.png");
 
+		adminService.signUpAdmin("admin", "password", "admin@gmail.com");
 	}
-        userService.signup("user1", "user11234@", "user1@gmail.com", "user1", "서울시 강남구", "https://example.com/default_profile.png");
-        userService.signup("user2", "user21234@", "user2@gmail.com", "user2", "서울시 강서구", "https://example.com/default_profile.png");
-        userService.signup("user3", "user31234@", "user3@gmail.com", "user3", "서울시 광진구", "https://example.com/default_profile.png");
 
-    }
 }
