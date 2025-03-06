2383861b7fbbebedd8a795314cc253334580f595
asungkim <asungkim007@naver.com>
2025-03-06 11:03:58 +0900

    [feat] 카카오 로그인 추가 * 프론트 홈 화면에서 카카오 로그인 버튼을 누르면 카카오 로그인으로 이동 * 카카오 정보를 통해 유저가 이미 있으면 nickname, profile_url만 바꿔줌 없으면 새로 회원가입 * accessToken, refreshToken 쿠키로 발급

--- backend/src/main/java/com/NBE_4_5_2/Team5/global/security/SecurityConfig.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/global/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
@@ -17,7 +18,8 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 public class SecurityConfig {
 
     private final CustomAuthenticationFilter customAuthenticationFilter;
-//    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomAuthorizationRequestResolver customAuthorizationRequestResolver;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -37,10 +39,14 @@ public class SecurityConfig {
                 .headers((headers) -> headers
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
                                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
                 .oauth2Login((oauth2) -> {
-                    oauth2.successHandler(((request, response, authentication) -> {
-                        response.sendRedirect("http://localhost:3000");
-                    }));
+                    oauth2.authorizationEndpoint(
+                            authorizationEndpoint -> authorizationEndpoint.authorizationRequestResolver(customAuthorizationRequestResolver)
+                    );
+                    oauth2.successHandler(customAuthenticationSuccessHandler);
                 })
                 .csrf(csrf -> csrf.disable())
                 .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
