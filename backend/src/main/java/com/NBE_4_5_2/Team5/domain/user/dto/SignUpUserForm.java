c540d9ef9360d1d49f2cbe1eb74ab7ad15d2897b
asungkim <asungkim007@naver.com>
2025-03-10 17:13:25 +0900

    [merge] 준식님 springDocs 코드 복사

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/user/dto/SignUpUserForm.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/user/dto/SignUpUserForm.java
@@ -6,10 +6,12 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record SignUpUserForm(
+        @NotBlank(message = "아이디는 필수 입력값입니다.")
         @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "아이디는 영문과 숫자만 사용할 수 있습니다.")
         @Size(min = 4, max = 20, message = "아이디는 4~20자 사이여야 합니다.")
         String username,
 
+        @NotBlank(message = "비밀번호는 필수 입력값입니다.")
         @Size(min = 8, max = 50, message = "비밀번호는 8~50자 사이여야 합니다.")
         @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
                 message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.")
