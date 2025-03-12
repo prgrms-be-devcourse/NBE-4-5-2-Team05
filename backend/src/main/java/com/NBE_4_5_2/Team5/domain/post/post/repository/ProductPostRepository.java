5110867b01716b5ddadb1760f67ac83748efa7e7
asungkim <asungkim007@naver.com>
2025-03-12 16:25:47 +0900

    [refactor] 상품 관리 리펙토링 * 응답 dto 데이터 추가 * 내 상품글 상태 추가 * 상태 추가에 따른 서비스 코드 변환

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/post/post/repository/ProductPostRepository.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/post/post/repository/ProductPostRepository.java
@@ -1,9 +1,8 @@
 package com.NBE_4_5_2.Team5.domain.post.post.repository;
 
 import com.NBE_4_5_2.Team5.domain.post.post.entity.ProductPost;
+import com.NBE_4_5_2.Team5.domain.post.post.enums.ProductStatus;
 import com.NBE_4_5_2.Team5.domain.user.entity.User;
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -12,9 +11,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
-import com.NBE_4_5_2.Team5.domain.post.post.entity.ProductPost;
-import com.NBE_4_5_2.Team5.domain.post.post.enums.ProductStatus;
-import com.NBE_4_5_2.Team5.domain.user.entity.User;
+import java.util.List;
 
 @Repository
 public interface ProductPostRepository extends JpaRepository<ProductPost, String> {
@@ -41,4 +38,6 @@ public interface ProductPostRepository extends JpaRepository<ProductPost, String
 	List<ProductPost> findByBuyer(User buyer);
 
 	List<ProductPost> findByIdIn(List<String> postIds);
+
+	Page<ProductPost> findByWriterAndStatus(User writer, ProductStatus status, Pageable pageable);
 }
