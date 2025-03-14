07750bef8f9eacbdd8472c0d7aa19343926e91e1
omegafrog <jiwoo22110103@gmail.com>
2025-03-14 20:01:21 +0900

    docs : admin docs 추가 및 인증

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/admin/controller/AdminController.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/admin/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.NBE_4_5_2.Team5.domain.admin.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
@@ -44,6 +46,8 @@ public class AdminController {
 
 	@Operation(summary = "공지사항 등록", description = "새로운 공지사항을 등록합니다.")
 	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "공지사항 등록 성공")})
+	@SecurityRequirement(name = "cookieAuth")
+	@PreAuthorize("isAuthenticated")
 	@PostMapping("/notices")
 	public RsData<NoticeResBody> writeNotice(
 		@Parameter(description = "공지사항 등록 body")
@@ -58,6 +62,8 @@ public class AdminController {
 
 	@Operation(summary = "유저 정지", description = "특정 유저를 정지시킵니다.")
 	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "유저 정지 성공")})
+	@SecurityRequirement(name = "cookieAuth")
+	@PreAuthorize("isAuthenticated")
 	@PostMapping("/users/{user-id}/ban")
 	public RsData<BanResBody> banUser(
 		@Parameter(description = "유저 id")
@@ -73,6 +79,8 @@ public class AdminController {
 
 	@Operation(summary = "게시글 삭제", description = "게시글을 삭제합니다.")
 	@ApiResponses(value = {@ApiResponse(responseCode = "204", description = "게시글 삭제 성공")})
+	@SecurityRequirement(name = "cookieAuth")
+	@PreAuthorize("isAuthenticated")
 	@DeleteMapping("/posts/{post-id}")
 	public RsData<Void> deletePost(
 		@Parameter(description = "post id")
