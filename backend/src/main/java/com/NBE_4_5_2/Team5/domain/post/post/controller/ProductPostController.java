a88b584566826df283121e8f55c2c365ed7a33d0
asungkim <asungkim007@naver.com>
2025-03-15 17:48:22 +0900

    [fix] myPosts, purchasePost, likePost page 추가

--- backend/src/main/java/com/NBE_4_5_2/Team5/domain/post/post/controller/ProductPostController.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/domain/post/post/controller/ProductPostController.java
@@ -1,23 +1,10 @@
 package com.NBE_4_5_2.Team5.domain.post.post.controller;
 
-import java.util.List;
-
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.NBE_4_5_2.Team5.domain.post.post.dto.request.ProductPostModifyForm;
 import com.NBE_4_5_2.Team5.domain.post.post.dto.request.ProductPostWriteForm;
 import com.NBE_4_5_2.Team5.domain.post.post.dto.response.PreviewPostResponse;
 import com.NBE_4_5_2.Team5.domain.post.post.dto.response.ProductPostResponse;
+import com.NBE_4_5_2.Team5.domain.post.post.enums.ProductStatus;
 import com.NBE_4_5_2.Team5.domain.post.post.service.ProductPostService;
 import com.NBE_4_5_2.Team5.domain.post.post.service.RecentlyViewedService;
 import com.NBE_4_5_2.Team5.domain.user.user.entity.User;
@@ -26,9 +13,13 @@ import com.NBE_4_5_2.Team5.global.Rq;
 import com.NBE_4_5_2.Team5.global.dto.Empty;
 import com.NBE_4_5_2.Team5.global.dto.PageDto;
 import com.NBE_4_5_2.Team5.global.dto.RsData;
-
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -71,11 +62,13 @@ public class ProductPostController {
 	@PreAuthorize("isAuthenticated()")
 	@GetMapping("/my")
 	@Transactional(readOnly = true)
-	public RsData<PageDto<PreviewPostResponse>> getMyPosts(@RequestParam(defaultValue = "1") int page,
-		@RequestParam(defaultValue = "10") int pageSize,
-		@RequestParam(defaultValue = "desc") String sort) {
+	public RsData<PageDto<PreviewPostResponse>> getMyPosts(
+			@RequestParam(defaultValue = "1") int page,
+			@RequestParam(defaultValue = "10") int pageSize,
+			@RequestParam(defaultValue = "desc") String sort,
+			@RequestParam(required = false)ProductStatus status) {
 		User actor = userAuthService.getUserIdentity();
-		PageDto<PreviewPostResponse> postPage = productPostService.getMyPosts(actor, page, pageSize, sort);
+		PageDto<PreviewPostResponse> postPage = productPostService.getMyPosts(actor, page, pageSize, sort,status);
 
 		return new RsData<>(
 			"200",
@@ -147,10 +140,13 @@ public class ProductPostController {
 
 	// 내가 구매한 내역 조회
 	@GetMapping("/my/purchases")
-	public RsData<List<ProductPostResponse>> getMyPurchases() {
+	public RsData<PageDto<ProductPostResponse>> getMyPurchases(
+			@RequestParam(defaultValue = "1") int page,
+			@RequestParam(defaultValue = "10") int pageSize
+	) {
 		User actor = userAuthService.getUserIdentity();
 
-		List<ProductPostResponse> myPurchases = productPostService.getMyPurchases(actor);
+		PageDto<ProductPostResponse> myPurchases = productPostService.getMyPurchases(actor,page,pageSize);
 
 		return new RsData<>(
 			"200",
@@ -176,9 +172,12 @@ public class ProductPostController {
 	/// 내가 찜한 내역
 	@PreAuthorize("isAuthenticated()")
 	@GetMapping("/my/favorites")
-	public RsData<List<ProductPostResponse>> getMyFavorites() {
+	public RsData<PageDto<ProductPostResponse>> getMyFavorites(
+			@RequestParam(defaultValue = "1") int page,
+			@RequestParam(defaultValue = "10") int pageSize
+	) {
 		User actor = userAuthService.getUserIdentity();
-		List<ProductPostResponse> favorites = productPostService.getMyFavorites(actor);
+		PageDto<ProductPostResponse> favorites = productPostService.getMyFavorites(actor,page,pageSize);
 
 		return new RsData<>(
 			"200",
