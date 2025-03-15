cc9dc1906edc4bf602863e8db2ac55fe77d8387a
jeonghyeon-ohk <ok6737@naver.com>
2025-03-15 17:02:23 +0900

    chore: develop 브랜치 최신 내용 반영

--- backend/src/main/java/com/NBE_4_5_2/Team5/global/security/CustomAuthenticationFilter.java
+++ backend/src/main/java/com/NBE_4_5_2/Team5/global/security/CustomAuthenticationFilter.java
@@ -1,23 +1,22 @@
 package com.NBE_4_5_2.Team5.global.security;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
 import com.NBE_4_5_2.Team5.domain.user.user.dto.AuthToken;
 import com.NBE_4_5_2.Team5.domain.user.user.entity.User;
 import com.NBE_4_5_2.Team5.domain.user.user.service.UserAuthService;
 import com.NBE_4_5_2.Team5.domain.user.user.service.UserService;
 import com.NBE_4_5_2.Team5.global.Rq;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
 
 @Component
 @RequiredArgsConstructor
